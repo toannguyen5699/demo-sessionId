2b8bbba20921805f6e327adc0747b924b4b857f7
Toan Nguyen <toannguyen050699@gmail.com>
2019-07-04 21:20:32 +0700

    Demo Session and defend CSRF Attack

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,40 +1,43 @@
-var db = require('../db');
-var shortid = require('shortid');
 var md5 = require('md5');
 
+var db = require('../db');
+
+
 module.exports.login = function(req, res) {
-	res.render('auth/login');
+  res.render('auth/login');
 };
 
 module.exports.postLogin = function(req, res) {
-	var email = req.body.email;
-	var password = req.body.password;
+  var email = req.body.email;
+  var password = req.body.password;
 
-	var user = db.get('users').find({ email: email}).value();
+  var user = db.get('users').find({ email: email }).value();
 
-	if (!user) {
-		res.render('auth/login', {
-			errors: [
-				'User does not exists'
-			],
-			values: req.body
-		});
-		return;
-	}
+  if (!user) {
+    res.render('auth/login', {
+      errors: [
+        'User does not exist.'
+      ],
+      values: req.body
+    });
+    return;
+  }
 
-	var hashedPassword = md5(password);
+  var hashedPassword = md5(password);
 
-	if (user.password !== hashedPassword) {
-		res.render('auth/login', {
-			errors: [
-				'Wrong password'
-			],
-			values: req.body
-		});
-		return;
-	}
-	res.cookie('userId', user.id, { 
-		signed: true 
-	});
-	res.redirect('/users');
+  if (user.password !== hashedPassword) {
+    res.render('auth/login', {
+      errors: [
+        'Wrong password.'
+      ],
+      values: req.body
+    });
+    return;
+  }
+
+  res.cookie('userId', user.id, {
+    signed: true
+  });
+
+  res.redirect('/users');
 };
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var csurf = require('csurf');
+
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
 var productRoute = require('./routes/product.route');
 var cartRoute = require('./routes/cart.route');
+var transferRoute = require('./routes/transfer.route');
 
 var authMiddleware = require('./validate/auth.middleware');
 var sessionMiddleware = require('./validate/session.middleware')
@@ -17,10 +20,12 @@ var app =express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware);
+app.use(csurf({ cookie: true}));
 
 app.use(express.static('public'));
 
@@ -34,6 +39,7 @@ app.use('/users',authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
 app.use('/products',productRoute);
 app.use('/cart', cartRoute);
+app.use('/transfer',authMiddleware.requireAuth, transferRoute);
 
 
 app.listen(port, function() {
